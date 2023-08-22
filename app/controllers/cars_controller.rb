c58ca0dabc5605dc161034873e8f606c0fcfe23a
Adrian Schober <adrian.schober1970@gmail.com>
2023-08-22 14:52:00 +0200

    Added the Create a Car form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,9 +3,27 @@ class CarsController < ApplicationController
 
   def show; end
 
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_car
     @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:model, :price, :seat_number, photos: [])
   end
 end
