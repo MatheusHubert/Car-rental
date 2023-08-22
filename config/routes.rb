7e5b537ef3b11101a217635a8632b8e4e8d7ac35
Matheus Hubert <matheushubert@gmail.com>
2023-08-22 15:43:38 +0200

    sign out fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "cars#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  
+  devise_for :users
+
   resources :cars, except: %I[index] do
     resources :bookings, only: %I[new create]
   end
