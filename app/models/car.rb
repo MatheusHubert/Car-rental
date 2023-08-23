84db51601e87a709aac14df90b1c97a128375263
Adrian Schober <adrian.schober1970@gmail.com>
2023-08-23 15:54:36 +0200

    Adding profile page and minor style changes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   has_many_attached :photos
   validates :model, :price, :seat_number, :description, :address, presence: true
